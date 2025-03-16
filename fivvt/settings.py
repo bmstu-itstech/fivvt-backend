124d0367e6fc39508f7b373da40b746254a9017c
zhikh <zhikh.k@gmail.com>
2025-03-16 20:40:25 +0300

    fix: move corsheaders.middleware to the MIDDLEWARE from APPS

--- fivvt/settings.py
+++ fivvt/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
-    'corsheaders.middleware.CorsMiddleware',
 
     'board',
     'albums',
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'fivvt.urls'
